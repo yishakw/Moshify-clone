4ac1413d7846d3ea0652d207f2e3e983ac81f08e
Yishak Wondimgezahu <yishakwondimgezahu12@gmail.com>
2023-10-29 16:09:34 +0300

    further change on nav bar

--- Moshified-clone/src/NavBar/Navbar.jsx
+++ Moshified-clone/src/NavBar/Navbar.jsx
@@ -1,11 +1,18 @@
+import { useState } from "react";
 import Logo from "./Logo";
 import styles from "./Navbar.module.css";
 export default function Navbar() {
+  const [hovered, setHovered] = useState(false);
   return (
     <div className={styles["nav_bar"]}>
       <Logo />
-      <ul>
-        <li>Hosting</li>
+      <ul style={{ color: hovered ? "white" : "wheat", cursor: "pointer" }}>
+        <li
+          onMouseOver={() => setHovered(true)}
+          onMouseOut={() => setHovered(false)}
+        >
+          Hosting
+        </li>
         <li>VPS</li>
         <li>Domain</li>
         <li>Pricing</li>
