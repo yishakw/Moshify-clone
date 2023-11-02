3942eef8e48876920be67c99694e6a666385fed3
Yishak Wondimgezahu <yishakwondimgezahu12@gmail.com>
2023-11-02 12:51:46 +0300

    further commits

--- Moshified-clone/src/App.jsx
+++ Moshified-clone/src/App.jsx
@@ -1,10 +1,12 @@
 import Navbar from "./NavBar/Navbar";
+import MainSection from "./Main/MainSection";
 import "./App.css";
 
 function App() {
   return (
     <>
       <Navbar />
+      <MainSection />
     </>
   );
 }
